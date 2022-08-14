ae159f53ad17528af035ebba111d8cad2413c8a1
Ujala <ujala.ikhar@neosoftmail.com>
2022-08-14 08:38:23 +0530

    Initial Commit

---
+++ imageapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from imageapp.models import Image
+
+# Create your views here.
+def imageView(request):
+    if request.method == 'POST':
+        formdata = request.POST
+        name = formdata.get('nm')
+        email = formdata.get('em')
+        age = formdata.get('ag')
+        img = Image(name = name,email = email, age = age)
+        img.save()
+    return render(request,template_name='index.html')
+
+
