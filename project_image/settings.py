e35af6d82de83aa9ad709de9cf2e03969bfe2d7a
Ujala <ujala.ikhar@neosoftmail.com>
2022-08-14 13:30:51 +0530

    Project Updated

--- project_image/settings.py
+++ project_image/settings.py
@@ -17,8 +17,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-env = environ.Env(SECRET_KEY=str,)
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env = environ.Env()
+environ.Env.read_env()
 
 
 # Quick-start development settings - unsuitable for production
